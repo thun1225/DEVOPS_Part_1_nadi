24bbb09a403f4133e3adb2a3333b6b0e61c72fa6
thun1225 <thunnadioo1205@gmail.com>
2024-10-25 19:02:10 +0800

    Implemented the frontend for add-resource

---
+++ public/js/hotel.js
@@ -0,0 +1,37 @@
+function addHotel() {
+    var response = "";
+    var jsonData = new Object();
+    jsonData.name = document.getElementById("name").value;
+    jsonData.location = document.getElementById("location").value;
+    jsonData.description = document.getElementById("description").value;
+    jsonData.owner = document.getElementById("owner").value;
+
+    if (jsonData.name == "" || jsonData.location == "" || jsonData.description == "") {
+        document.getElementById("message").innerHTML = 'All fields are required!';
+        document.getElementById("message").setAttribute("class", "text-danger");
+        return;
+    }
+
+    var request = new XMLHttpRequest();
+    request.open("POST", "/add-hotel", true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.onload = function () {
+        response = JSON.parse(request.responseText);
+        console.log(response);
+
+        if (response.message == undefined) {
+            document.getElementById("message").innerHTML = 'Added Hotel: ' + jsonData.name + '!';
+            document.getElementById("message").setAttribute("class", "text-success");
+            document.getElementById("name").value = "";
+            document.getElementById("location").value = "";
+            document.getElementById("description").value = "";
+            document.getElementById("owner").value = "";
+            window.location.href = 'index.html';
+        } else {
+            document.getElementById("message").innerHTML = 'Unable to add hotel!';
+            document.getElementById("message").setAttribute("class", "text-danger");
+        }
+    };
+
+    request.send(JSON.stringify(jsonData));
+}
