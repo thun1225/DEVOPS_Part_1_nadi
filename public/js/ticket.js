be3f9a09a32d9f6d55aa67750ec18d8b50601416
ragaswarupaG <ragaswarupa.g@gmail.com>
2024-10-25 17:02:10 +0800

    Implemented the frontend with no css

---
+++ public/js/ticket.js
@@ -0,0 +1,73 @@
+function addTrainTicket() {
+  var response = "";
+  var jsonData = new Object();
+
+
+
+
+
+  jsonData.name = document.getElementById("name").value;
+  jsonData.email = document.getElementById("email").value;
+  jsonData.phoneNumber = document.getElementById("phoneNumber").value;
+  jsonData.quantity = document.getElementById("quantity").value;
+  jsonData.location = document.getElementById("location").value;
+  jsonData.dateOfBooking = document.getElementById("dateOfBooking").value;
+  jsonData.dateOfTravel = document.getElementById("dateOfTravel").value;
+  jsonData.cardNumber = document.getElementById("cardNumber").value;
+
+
+
+
+  if (
+    jsonData.name == "" ||
+    jsonData.email == "" ||
+    jsonData.phoneNumber == "" ||
+    jsonData.quantity == "" ||
+    jsonData.location == "" ||
+    jsonData.dateOfBooking == "" ||
+    jsonData.dateOfTravel == "" ||
+    jsonData.cardNumber == ""
+ 
+  ) {
+    document.getElementById("message").innerHTML = "All fields are required!";
+    document.getElementById("message").setAttribute("class", "text-danger");
+    return;
+  }
+  var request = new XMLHttpRequest();
+  request.open("POST", "/add-ticket-booking", true);
+  request.setRequestHeader("Content-Type", "application/json");
+  request.onload = function () {
+    response = JSON.parse(request.responseText);
+    console.log(response);
+    if (response.message == undefined) {
+      document.getElementById("message").innerHTML =
+        "Created ticket booking: " + jsonData.name + "!";
+      document.getElementById("message").setAttribute("class", "text-success");
+
+
+
+      
+document.getElementById("name").value = "";
+ document.getElementById("email").value= "";
+ document.getElementById("phoneNumber").value = "";
+  document.getElementById("quantity").value = "";
+document.getElementById("location").value = "";
+ document.getElementById("dateOfBooking").value = "";
+  document.getElementById("dateOfTravel").value = "";
+ document.getElementById("cardNumber").value = "";
+
+
+
+
+
+
+
+
+    } else {
+      document.getElementById("message").innerHTML = "Unable to add resource!";
+      document.getElementById("message").setAttribute("class", "text-danger");
+      document.getElementById("message").setAttribute("class", "text-danger");
+    }
+  };
+  request.send(JSON.stringify(jsonData));
+}
